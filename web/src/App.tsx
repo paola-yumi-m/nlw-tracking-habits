9f25784478b08e836b991cd35192a7bce776ef3e
paola-yumi-m <paola.yumi.m@gmail.com>
2023-01-26 08:34:13 -0300

    front-end gerando quadradinhos dos dias

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,6 @@
-import { Habit } from './components/Habit'
+// import { HabitDay } from './components/HabitDay'
 import { Header } from './components/Header'
+import { SummaryTable } from './components/SummaryTable'
 import './styles/global.css'
 
 export function App() {
@@ -8,6 +9,7 @@ export function App() {
     <div className='w-screen h-screen flex justify-center items-center'>
       <div className='w-full max-w-5xl px-6 flex flex-col gap-16'>
         <Header />
+        <SummaryTable />
       </div>
     </div>
   )
