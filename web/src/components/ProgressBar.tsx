6632f4ed6b7aeba8ae6c670d25936777acccfa92
paola-yumi-m <paola.yumi.m@gmail.com>
2023-02-07 09:08:02 -0300

    web: adicionando checkboxes e coletando dados do form de novo hábito.

--- web/src/components/ProgressBar.tsx
+++ web/src/components/ProgressBar.tsx
@@ -4,12 +4,12 @@ interface ProgressBarProps {
 
 export function ProgressBar(props: ProgressBarProps) {
   return (
-    <div className="h-3 rounded-xl bg-zinc-700 w-full mt-4">
+    <div className='h-3 rounded-xl bg-zinc-700 w-full mt-4'>
       <div
-        role="progressbar"
-        aria-label="Progresso de hábitos completados nesse dia"
-        aria-aria-valuenow={props.progress}
-        className="h-3 rounded-xl bg-violet-600"
+        role='progressbar'
+        aria-label='Progresso de hábitos completados nesse dia'
+        aria-valuenow={props.progress}
+        className='h-3 rounded-xl bg-violet-600'
         style={{
           width: `${props.progress}%`,
         }}
