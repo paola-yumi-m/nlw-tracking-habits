37567a72252769b55764fcc78db642767e6a477b
paola-yumi-m <paola.yumi.m@gmail.com>
2023-02-24 08:47:54 -0300

    web: adicionando animações e focus

--- web/src/components/HabitsList.tsx
+++ web/src/components/HabitsList.tsx
@@ -63,13 +63,16 @@ export function HabitsList({ date, onCompletedChanged }: HabitsListProps) {
     <div className='mt-6 flex flex-col gap-3'>
       {habitsInfo?.possibleHabits.map((habit) => (
         <Checkbox.Root
-          className='flex items-center gap-3 group'
+          className='flex items-center gap-3 group focus:outline-none disabled:cursor-not-allowed'
           key={habit.id}
           onCheckedChange={() => handleToggleChecked(habit.id)}
           checked={habitsInfo.completedHabits.includes(habit.id)}
           disabled={isDayInPast}
         >
-          <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
+          <div
+            className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500 transition-colors
+          group-focus:ring-2 group-focus:ring-violet-600 group-focus:ring-offset-2 group-focus:ring-offset-background'
+          >
             <Checkbox.Indicator>
               <Check
                 size={20}
